2315f9c3cfe9c0e28b38849722a224f35d6dd044
Craig Spencer <craigspencer.linux@gmail.com>
2023-04-13 10:51:21 -0700

    removes out and package-lock

--- out/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Intel Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-//# sourceMappingURL=types.js.map
