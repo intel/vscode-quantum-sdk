1d990eb2d0a31cd48419bf64a25f019e6006cdb7
Rolando1010 <86446201+Rolando1010@users.noreply.github.com>
2023-04-13 12:02:37 -0600

    chore: node_modules folder, out folder and package-lock.json added to gitignore (#10)
    * chore: node_modules folder added to gitignore
    
    Signed-off-by: Rolando1010 <rmcdesarrollo@gmail.com>
    
    * removes out and package-lock
    
    * adds out and package-lock to gitignore
    
    * adds npm install step to docs
    
    ---------
    
    Signed-off-by: Rolando1010 <rmcdesarrollo@gmail.com>
    Co-authored-by: Craig Spencer <craigspencer.linux@gmail.com>

--- out/model.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// This file contains a list of function that will be used
-// to translate a quantum C++ program into a visual 
-// quantum circuit board whenever the file is saved
-//# sourceMappingURL=model.js.map
