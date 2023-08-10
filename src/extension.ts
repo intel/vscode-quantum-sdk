21daa246674c7af5e12e2fe5d487d081a49e09a5
Craig Spencer <craig.spencer@intel.com>
2023-08-10 13:52:06 -0700

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let dir = vscode.workspace.workspaceFolders![0].uri.path
 
-    	//promise based system shell commnd, hoist if deemed useful elsewhere
-    	const subShell = (cmd: string ) => new Promise<string>((resolve, reject) => {
-    	    subp.exec(cmd, (err, out) => {
-    	        if (err) return reject(err);
-    	        return resolve(out);
-    	    });
-    	});
+	//promise based system shell commnd, hoist if deemed useful elsewhere
+	const subShell = (cmd: string) => new Promise<string>((resolve, reject) => {
+		subp.exec(cmd, (err, out) => {
+			if (err) return reject(err);
+			return resolve(out);
+		});
+	});
 
 	updateCustomContext(vscode.window.activeTextEditor)
 	vscode.window.onDidChangeActiveTextEditor(editor => { updateCustomContext(editor) })
@@ -59,18 +59,16 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 	context.subscriptions.push(vscode.commands.registerCommand(setupCommand, setup))
 
-	const execDrawRoutine = ( circuitData: string ) => {
-        	try {
-        	    let data: QData = JSON.parse(circuitData) as QData
-        	    CircuitPanel.validateQData(data)
-        	    CircuitPanel.displayWebview(context.extensionUri, data, true)
-        	    //vscode.ViewColumn.One
-        	} catch (e) {
-        	    let dataError: QData = { title: (e as Error).message } as QData
-        	    CircuitPanel.displayWebview(context.extensionUri, dataError, false)
-        	    //vscode.ViewColumn.One
-        	}
-    	}
+	const execDrawRoutine = (circuitData: string) => {
+		try {
+			let data: QCircuitData = JSON.parse(circuitData) as QCircuitData
+			CircuitPanel.validateQCircuitData(data)
+			CircuitPanel.displayCircuitWebview(context.extensionUri, data, true)
+		} catch (e) {
+			let dataError: QCircuitData = { title: (e as Error).message } as QCircuitData
+			CircuitPanel.displayCircuitWebview(context.extensionUri, dataError, false)
+		}
+	}
 
 	const drawCircuitCommand = "intel-quantum.drawCircuit"
 	const drawCircuit = () => {
@@ -79,19 +77,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 		if (editor !== undefined) {
 			if (editor.document.languageId === 'json') {
-                		execDrawRoutine( editor.document.getText() )
+				execDrawRoutine(editor.document.getText())
 			} else if (editor.document.languageId === 'cpp') {
-                		const name = editor.document.fileName.split('.').slice(0, -1).join('.').split('/').pop()
-                		subShell( "cd /home/workspace && make -f /home/iqsdk_user/Makefile NAME="+name ).then( () => {
-                			const cursorPosition = editor.selection.active;
-                			const lineText = editor.document.lineAt(cursorPosition.line).text;
-                			let KernelName = "MainCircuit";
+				const name = editor.document.fileName.split('.').slice(0, -1).join('.').split('/').pop()
+				subShell("cd /home/workspace && make -f /home/iqsdk_user/Makefile NAME=" + name).then(() => {
+					const cursorPosition = editor.selection.active;
+					const lineText = editor.document.lineAt(cursorPosition.line).text;
+					let KernelName = "MainCircuit";
 
-                			subShell("cat /home/workspace/Visualization/*MainCircuit*.json").then((out)=> {
-                        	    		//console.log(out)
-                				execDrawRoutine(out)
-                			})
-                		})
+					subShell("cat /home/workspace/Visualization/*MainCircuit*.json").then((out) => {
+						//console.log(out)
+						execDrawRoutine(out)
+					})
+				})
 			}
 		} else {
 			console.log("No Active Editor")
@@ -105,13 +103,6 @@ export function activate(context: vscode.ExtensionContext) {
 		let fileContent: string = ''
 
 		if (editor !== undefined) {
-			if (editor.document.languageId === 'json') {
-				fileContent = editor.document.getText()
-			}
-			// else if (editor.document.languageId === 'cpp') {
-
-			// }
-
 			try {
 				let data: QHistogramData = JSON.parse(fileContent) as QHistogramData
 				CircuitPanel.validateQHistogramData(data)
@@ -155,13 +146,14 @@ function updateCustomContext(editor: vscode.TextEditor | undefined) {
 		}
 	} else if (editor && editor.document.languageId === "cpp") {
 		let editorText = editor.document.getText()
-        //const regex = new RegExp('^[^\S\r\n]*#[^\S\r\n]*include[^\S\r\n]*<[^\S\r\n]*quantum.hpp[^\S\r\n]*>[^\S\r\n]*$');
-        const regex = new RegExp('#include[^\S\r\n]*<[^\S\r\n]*quantum.hpp[^\S\r\n]*>', 'g');
-		if( regex.test(editorText) ){ 
-		//if (editorText.includes('#include <quantum.hpp>')) { 
-			vscode.commands.executeCommand('setContext', 'customContext.quantumFile', true)
+		//const regex = new RegExp('^[^\S\r\n]*#[^\S\r\n]*include[^\S\r\n]*<[^\S\r\n]*quantum.hpp[^\S\r\n]*>[^\S\r\n]*$');
+		const regex = new RegExp('#include[^\S\r\n]*<[^\S\r\n]*quantum.hpp[^\S\r\n]*>', 'g');
+		if (regex.test(editorText)) {
+			//if (editorText.includes('#include <quantum.hpp>')) { 
+			vscode.commands.executeCommand('setContext', 'customContext.quantumCPPScript', true)
 			return
 		}
+	}
 }
 
 export function deactivate() { }
